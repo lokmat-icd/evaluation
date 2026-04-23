dea4d754144c399de0ef679ca43a4c7eb119ae0b
lokmat-icd <icdlokmat@gmail.com>
2026-04-23 22:54:26 +0530

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 // ==================== CONFIGURATION ====================
 // IMPORTANT: Replace this URL with your ACTUAL deployed Apps Script URL
 // After deploying your GS-Code.txt as a Web App, copy the URL here
-const API_URL = "https://script.google.com/macros/s/AKfycbw4hlT61G0BD9Oodla2amfqEdeNfkD2SnSc3Ppj4rWnd2btV7oixDwxr1mgXsFyd7if/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx23IC95oWfaoREj106mJ-lAeEvxgwzQ-4qIJoz0xayuBvVXhoj0Xqp0ImVFxFplIJw/exec";
 
 // ==================== GLOBAL VARIABLES ====================
 let authenticatedUser = null;      // For ICD/Head/Vertical Head
